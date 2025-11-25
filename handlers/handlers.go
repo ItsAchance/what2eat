56b93496bb95cb54f2b023167de031ace180a2f5
ItsAchance <contact@alexanderchance.me>
2025-11-25 20:58:24 +0100

    Hello world complete

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/ItsAchance/what2eat/components"
 )
 
-
+func Startpage(w http.ResponseWriter, r *http.Request) {
+	component := components.Base("You nerd")
+	component.Render(r.Context(), w)
+}
--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/ItsAchance/what2eat/handlers"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /", handlers.Startpage)
+
 	log.Print("Listening on http://localhost:3000")
 	http.ListenAndServe(":3000", mux)
 }
-
